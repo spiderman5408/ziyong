f81105d584fd06106339e8726934e5401954511b
643822883 <33469503+643822883@users.noreply.github.com>
2017-11-26 20:07:23 +0800

    Update zi.sh

--- zi.sh
+++ zi.sh
@@ -195,15 +195,17 @@ fi
 #一键安装libsodium
 if test $os == 16 ;then
 echo '******正在安装libsodium，请等待几分钟******';
-wget -N --no-check-certificate https://raw.githubusercontent.com/643822883/daige/master/libsodium/libsodium.sh;
+wget -N --no-check-certificate ${url}/libsodium.sh;
 chmod +x libsodium.sh;
 bash libsodium.sh;
+fi
 
 #一键安装ariaNG和filemanger
 if test $os == 17 ;then
 echo '******正在安装ariaNG和filemanger，请等待几分钟******';
-wget -N --no-check-certificate https://raw.githubusercontent.com/643822883/daige/master/ariaNGandfilemanger.sh;
+wget -N --no-check-certificate ${url}/ariaNGandfilemanger.sh;
 chmod +x ariaNGandfilemanger.sh;
 bash ariaNGandfilemanger.sh;
+fi
 
 exit 0
