d25e757d0d662aa71ff6e0b57ed075b919fd66b3
643822883 <33469503+643822883@users.noreply.github.com>
2017-11-26 19:55:16 +0800

    Add files via upload

---
+++ ariaNG+filemanger.sh
@@ -0,0 +1,50 @@
+
+
+#新建虚拟主机文件夹
+mkdir /usr/local/caddy/www;
+mkdir /usr/local/caddy/www/aria2;
+
+
+#写入到caddy配置文件
+echo ":88 {
+root /usr/local/caddy/www/aria2
+timeouts none
+gzip
+#basicauth / apple apple
+#browse
+filemanager /Download ./usr/local/caddy/www/aria2/Download {
+    show /usr/local/caddy/www/aria2/Download
+  # apple:
+    no_auth
+    locale zh-cn
+    allow_new true
+    allow_edit true
+    allow_publish true
+    allow_commands true
+    allow_command mv
+}
+}" > /usr/local/caddy/Caddyfile;
+
+
+# 新建Aria2下载文件夹 并进入文件夹
+mkdir /usr/local/caddy/www/aria2/Download;
+cd /usr/local/caddy/www/aria2;
+
+
+#下载并解压ariaNG文件
+wget -N --no-check-certificate "https://raw.githubusercontent.com/643822883/daige/master/ariaNG/aria-ng-0.3.0.zip";
+unzip aria-ng-0.3.0.zip;
+rm -rf aria-ng-0.3.0.zip;
+
+
+#赋予虚拟主机文件夹权限
+chmod -R 755 /usr/local/caddy/www/aria2;
+
+#启动caddy
+/etc/init.d/caddy start;
+service caddy restart;
+
+
+echo '安装成功，打开http://vps_ip:88/Download/files即可管理文件
+http://vps_ip:88管理ariaNG ';
+
