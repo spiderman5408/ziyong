59fefd68798170e4d806964978399e29ba0d223c
643822883 <33469503+643822883@users.noreply.github.com>
2018-11-10 05:13:44 +0800

    Add files via upload

---
+++ Rclone.sh
@@ -0,0 +1,53 @@
+#安装EPEL源：
+yum -y install epel-release
+
+#安装一些基本组件和依赖：
+
+yum -y install wget unzip screen fuse fuse-devel
+
+#下载Rclone解压然后进入目录
+wget --no-check-certificate -N "https://raw.githubusercontent.com/643822883/daige/master/Rclone/rclone-v1.42-linux-amd64.zip"
+unzip rclone-v1.42-linux-amd64.zip
+cd rclone-v1.42-linux-amd64
+
+#开始配置
+echo -e "第一步选择n，然后回车输入 lala"
+echo -e "然后选择我们要挂载的类型"
+echo -e "接着client_id、client_secret、service_account_file都留空直接回车，Use auto config?这里我们选择n"
+echo -e "按提示输入云盘授权码"
+echo -e "最后依次输入n、y、q"
+./rclone config
+
+#新建挂载目录
+mkdir -p /lala/gdrive
+
+#挂载目录为磁盘
+./rclone mount lala: /lala/gdrive --allow-other --allow-non-empty --vfs-cache-mode writes
+
+#新建一个rclone.service文件
+mkdir -p /usr/lib/systemd/system
+
+#把rclone的可执行文件复制到/usr/bin
+cp /root/rclone-v1.42-linux-amd64/rclone /usr/bin/rclone
+
+cd /usr/lib/systemd/system
+
+wget --no-check-certificate -N "https://raw.githubusercontent.com/643822883/daige/master/Rclone/rclone.service" 
+
+#重载daemon
+systemctl daemon-reload
+
+#用systemctl来启动rclone
+systemctl start rclone
+
+#设置开机启动
+systemctl enable rclone
+
+#配置自动上传到云盘
+cd /root
+
+wget --no-check-certificate -N "https://raw.githubusercontent.com/643822883/daige/master/Rclone/autoupload.sh"
+
+chmod +x /root/autoupload.sh
+
+echo "on-download-complete=/root/autoupload.sh" >> /root/.aria2/aria2.conf
