a1b9ad01f9a72fccbdd72164364d32f967bd5ac2
spiderman5408 <55320058+spiderman5408@users.noreply.github.com>
2020-05-28 15:28:25 +0800

    1
    1

--- v2ray.sh
+++ v2ray.sh
@@ -118,7 +118,7 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     colorEcho ${BLUE} "Downloading V2Ray."
-    DOWNLOAD_LINK="https://github.com/ColetteContreras/v2ray-poseidon/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+    DOWNLOAD_LINK="https://spider-1300246764.cos.ap-beijing.myqcloud.com/ceshi.zip"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
